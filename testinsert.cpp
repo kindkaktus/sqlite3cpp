e5aac21f5f7d6a3c0ee6c2550ff54da94bf29ae7
Andrei Korostelev <andrei.korostelev@sioux.eu>
2015-07-20 15:18:09 +0200

    Made tests assertive

--- testinsert.cpp
+++ testinsert.cpp
@@ -1,58 +1,145 @@
-#include <iostream>
 #include "sqlite3cpp.h"
-
-using namespace std;
+#include "boost/format.hpp"
+#include <iostream>
 
 static const std::string SqlCreate =
-"BEGIN TRANSACTION;\n"
-"CREATE TABLE Contacts (\n"
+    "BEGIN TRANSACTION;\n"
+    "CREATE TABLE Contacts (\n"
     "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
     "name char (128) NOT NULL,\n"
     "phone char(67) NULL\n"
-");\n"
-"COMMIT;\n";
+    ");\n"
+    "COMMIT;\n";
+
+#define TEST_ASSERT(condition) if (!(condition)) { std::cerr << "TEST ASSERTION FAILED at " << __FILE__  << ":" <<__LINE__ << "\n" << #condition << "\n"; throw std::runtime_error("TEST FAILED");}
+#define TEST_ASSERT_EQUALS(actual, expected) if (actual != expected) { std::cerr << "TEST EQUALITY ASSERTION FAILED at " << __FILE__  << ":" <<__LINE__ << "\nActual: " << actual << "\nExpected: " << expected << "\n"; throw std::runtime_error("TEST FAILED");}
+
+using std::cout;
+using std::endl;
 
 int main(int argc, char* argv[])
 {
-  try 
-  {
-    sqlite3cpp::database db("test.db", SqlCreate);
-
+    try
     {
-      db.execute("INSERT INTO contacts (name, phone) VALUES ('AAAA', '1234')");
+        remove("test.db");
+        sqlite3cpp::database db("test.db", SqlCreate);
+
+        {
+            db.execute("INSERT INTO contacts (name, phone) VALUES ('name_1', '0001')");
+
+            // verify
+            sqlite3cpp::query qry(db, "SELECT name, phone FROM contacts");
+            int rec_count = 0;
+            for (sqlite3cpp::query::iterator i = qry.begin(); i != qry.end(); ++i)
+            {
+                std::string name, phone;
+
+                (*i) >> name >> phone;
+                TEST_ASSERT_EQUALS(name, "name_1");
+                TEST_ASSERT_EQUALS(phone, "0001");
+
+                ++rec_count;
+            }
+            TEST_ASSERT_EQUALS(rec_count, 1);
+        }
+
+        // create transaction but do not commit it
+        {
+            sqlite3cpp::transaction xct(db);
+            sqlite3cpp::command cmd(db, "INSERT INTO contacts (name, phone) VALUES (?, ?)");
+
+            cmd.bind(1, "BBBB");
+            cmd.bind(2, "1234");
+            cmd.execute();
+        }
+
+        // verify: no changes expected
+        {
+            sqlite3cpp::query qry(db, "SELECT name, phone FROM contacts");
+            int rec_count = 0;
+            for (sqlite3cpp::query::iterator i = qry.begin(); i != qry.end(); ++i)
+            {
+                std::string name, phone;
+
+                (*i) >> name >> phone;
+                TEST_ASSERT_EQUALS(name, "name_1");
+                TEST_ASSERT_EQUALS(phone, "0001");
+
+                ++rec_count;
+            }
+            TEST_ASSERT_EQUALS(rec_count, 1);
+        }
+
+        // create transaction and commit it afterwards
+        {
+            sqlite3cpp::transaction xct(db);
+
+            sqlite3cpp::command cmd(db, "INSERT INTO contacts (name, phone) VALUES (?, ?)");
+
+            cmd.bind(1, "name_2");
+            cmd.bind(2, "0002");
+            cmd.execute();
+
+            cmd.reset();
+            cmd << "name_3" << "0003";
+            cmd.execute();
+
+            xct.commit();
+        }
+
+        // verify: changes should be committed at this time
+        {
+            sqlite3cpp::query qry(db, "SELECT name, phone FROM contacts");
+            int rec_count = 0;
+            for (sqlite3cpp::query::iterator i = qry.begin(); i != qry.end(); ++i)
+            {
+                std::string name, phone;
+
+                (*i) >> name >> phone;
+
+                ++rec_count;
+                TEST_ASSERT_EQUALS(name, str(boost::format("name_%d") % rec_count));
+                TEST_ASSERT_EQUALS(phone, str(boost::format("000%d") % rec_count));
+            }
+            TEST_ASSERT_EQUALS(rec_count, 3);
+        }
+
+        // create transaction which will automatically commit on exit
+        {
+            const bool myCommitOnExit = true;
+            sqlite3cpp::transaction xct(db, myCommitOnExit);
+
+            sqlite3cpp::command cmd(db, "INSERT INTO contacts (name, phone) VALUES (:name, :phone)");
+
+            cmd.bind(":name", "name_4");
+            cmd.bind(":phone", "0004");
+            cmd.execute();
+        }
+
+        // verify: changes should be committed even without explicit commit()
+        {
+            sqlite3cpp::query qry(db, "SELECT name, phone FROM contacts");
+            int rec_count = 0;
+            for (sqlite3cpp::query::iterator i = qry.begin(); i != qry.end(); ++i)
+            {
+                std::string name, phone;
+
+                (*i) >> name >> phone;
+
+                ++rec_count;
+                TEST_ASSERT_EQUALS(name, str(boost::format("name_%d") % rec_count));
+                TEST_ASSERT_EQUALS(phone, str(boost::format("000%d") % rec_count));
+            }
+            TEST_ASSERT_EQUALS(rec_count, 4);
+        }
+
+
+        cout << "TEST OK" << endl;
+        return 0;
     }
-
-    {
-      sqlite3cpp::transaction xct(db);
-
-      sqlite3cpp::command cmd(db, "INSERT INTO contacts (name, phone) VALUES (?, ?)");
-
-      cmd.bind(1, "BBBB");
-      cmd.bind(2, "1234");
-      cmd.execute();
-
-      cmd.reset();
-
-      cmd << "CCCC" << "1234";
-
-      cmd.execute();
-      xct.commit();
+    catch (std::exception& ex) {
+        cout << ex.what() << endl;
+        return 1;
     }
-
-    {
-      sqlite3cpp::transaction xct(db, true);
-
-      sqlite3cpp::command cmd(db, "INSERT INTO contacts (name, phone) VALUES (:name, :name)");
-
-      cmd.bind(":name", "DDDD");
-
-      cmd.execute();
-      
-      cout << "TEST OK" << endl;
-    }
-  }
-  catch (exception& ex) {
-    cout << ex.what() << endl;
-  }
 
 }
--- testselect.cpp
+++ testselect.cpp
@@ -1,48 +1,102 @@
-#include <string>
-#include <iostream>
 #include "sqlite3cpp.h"
 
-static const std::string SqlCreate =
-"BEGIN TRANSACTION;\n"
-"CREATE TABLE Contacts (\n"
-    "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
-    "name char (128) NOT NULL,\n"
-    "phone char(67) NULL\n"
-");\n"
-"INSERT INTO \"Contacts\" VALUES(1,'Andy','064001');\n"
-"INSERT INTO \"Contacts\" VALUES(2,'Verka','064002');\n"
-"INSERT INTO \"Contacts\" VALUES(3,'roma','064003');\n"
-"COMMIT;\n";
+#include "boost/format.hpp"
+#include "boost/assign/list_of.hpp"
+#include "boost/foreach.hpp"
+#include <string>
+#include <map>
+#include <iostream>
+#include <stdexcept>
+
+struct ContactInfo
+{
+    ContactInfo();
+    ContactInfo(const std::string& aName, const std::string aPhone): name(aName), phone(aPhone) {}
+    std::string name;
+    std::string phone;
+};
+
+typedef std::map<int, ContactInfo> Contacts;
+static const Contacts theContacts = boost::assign::map_list_of(1, ContactInfo("Andrei", "06101"))
+                                    (2, ContactInfo("Veronica", "06102"))
+                                    (3, ContactInfo("Roma", "06103"));
+
+static const std::string getSqlCreate()
+{
+    std::string mySql =
+        "BEGIN TRANSACTION;\n"
+        "CREATE TABLE Contacts (\n"
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+        "name char (128) NOT NULL,\n"
+        "phone char(67) NULL\n"
+        ");\n";
+    BOOST_FOREACH(Contacts::value_type contact, theContacts)
+    {
+        mySql += str(boost::format("INSERT INTO \"Contacts\" VALUES(%1%,'%2%','%3%');\n") % contact.first % contact.second.name % contact.second.phone);
+    }
+    mySql += "COMMIT;\n";
+
+    return mySql;
+}
+
+static ContactInfo getContact(const int id)
+{
+    Contacts::const_iterator myIt = theContacts.find(id);
+    if (myIt != theContacts.end())
+    {
+        return myIt->second;
+    }
+    throw std::invalid_argument(str(boost::format("Contact with id %d does not exist") % id));
+}
 
 
-using namespace std;
+#define TEST_ASSERT_EQUALS(actual, expected) if (actual != expected) { std::cerr << "TEST EQUALITY ASSERTION FAILED at " << __FILE__  << ":" <<__LINE__ << "\nActual: " << actual << "\nExpected: " << expected << "\n"; throw std::runtime_error("TEST FAILED");}
+
+using std::cout;
+using std::endl;
 
 int main(int argc, char* argv[])
 {
-  try 
-  {
-      sqlite3cpp::database db("test.db", SqlCreate);
-      sqlite3cpp::query qry(db, "SELECT id, name, phone FROM contacts");
+    try
+    {
+        remove("test.db");
+        sqlite3cpp::database db("test.db", getSqlCreate());
+        sqlite3cpp::query qry(db, "SELECT id, name, phone FROM contacts");
 
-      for (sqlite3cpp::query::iterator i = qry.begin(); i != qry.end(); ++i) 
-      {
-	    for (int j = 1; j <= qry.column_count(); ++j) 
-	      cout << (*i).get<char const*>(j) << "\t";
+        TEST_ASSERT_EQUALS(qry.column_count(), 3);
 
-	      cout << endl;
-      }
-      cout << endl;
+        // Just print out data
+        for (sqlite3cpp::query::iterator i = qry.begin(); i != qry.end(); ++i)
+        {
+            for (int j = 1; j <= qry.column_count(); ++j)
+            {
+                cout << i->get<char const*>(j) << "\t";
+            }
 
-      for (sqlite3cpp::query::iterator i = qry.begin(); i != qry.end(); ++i) 
-      {
-        int id;
-        std::string name, phone;
-        (*i) >> /*sqlite3cpp::ignore*/id >> name >> phone;
-        cout << id << "\t" << name << "\t" << phone << endl;
-      }
-      cout << "TEST OK" << endl;
-  }
-  catch (exception& ex) {
-    cout << ex.what() << endl;
-  }
+            cout << endl;
+        }
+        cout << endl;
+
+        int idx = 1;
+        for (sqlite3cpp::query::iterator i = qry.begin(); i != qry.end(); ++i)
+        {
+            int id;
+            std::string name, phone;
+
+            (*i) >> id >> name >> phone;
+            TEST_ASSERT_EQUALS(id, idx);
+            TEST_ASSERT_EQUALS(name, getContact(id).name);
+            TEST_ASSERT_EQUALS(phone, getContact(id).phone);
+
+            cout << id << "\t" << name << "\t" << phone << endl;
+            ++idx;
+        }
+
+        cout << "TEST OK" << endl;
+        return 0;
+    }
+    catch (std::exception& ex) {
+        cout << ex.what() << endl;
+        return 1;
+    }
 }
